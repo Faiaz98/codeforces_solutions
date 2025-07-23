a68bcfb43871ffb55baa41608f8b50e5dd310685
Md. Iram Faiaz Khan <76048711+Faiaz98@users.noreply.github.com>
2025-07-24 01:57:43 +0600

    Create c_prepend_and_append.cpp

---
+++ c_prepend_and_append.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        int l = 0, r = n - 1;
+        while (l < r && ((s[l] == '0' && s[r] == '1') || (s[l] == '1' && s[r] == '0'))) {
+            l++;
+            r--;
+        }
+
+        cout << r - l + 1 << endl;
+    }
+    return 0;
+}
